2b708bfdb37f438448cb1d59fa4a2e7271b51383
shonenada <shonenada@gmail.com>
2017-02-23 01:27:21 +0800

    feat(core): now we can use `nrepl run` start a clojure project

--- nrepl/core.py
+++ nrepl/core.py
@@ -3,13 +3,17 @@ from __future__ import print_function
 import os
 import sys
 import time
+import errno
 import socket
+import signal
 import argparse
+import threading
 import subprocess
 
 # compatibility support
 
 PY2 = sys.version_info[0] == 2
+
 
 if not PY2:
     text_type = str
@@ -17,7 +21,6 @@ if not PY2:
     unichr = chr
 
     from urllib.parse import urlparse
-
 else:
     text_type = unicode
     string_types = (str, unicode)
@@ -26,10 +29,7 @@ else:
     from urlparse import urlparse
 
 
-DEFAULT_nREPL_URI = "nrepl://localhost:31415"
-
-
-_read_byte = lambda s: s.read(1)
+DEFAULT_nREPL_URI = "nrepl://localhost:59258"
 
 
 def _write_data(fd, data):
@@ -66,13 +66,6 @@ class BencodeIO(object):
         return _write_data(self.fd, data)
 
 
-def nrepl_connect(uri):
-    uri = urlparse(uri)
-    _socket = socket.create_connection(uri.netloc.split(":"))
-    _fd = _socket.makefile('rw')
-    return BencodeIO(_fd, on_close=_socket.close)
-
-
 def get_ns_declare(fd):
     while True:
         line = fd.readline()
@@ -100,11 +93,27 @@ def traverse_dir(dirpath, callback):
             traverse_dir(dirpath, callback)
 
 
+def nrepl_connect(uri):
+    uri = urlparse(uri)
+    _socket = socket.create_connection(uri.netloc.split(":"))
+    _fd = _socket.makefile('rw')
+    return BencodeIO(_fd, on_close=_socket.close)
+
+
+def get_ts():
+    return int(time.time())
+
+
+class TimeoutError(Exception):
+    pass
+
+
 class Watcher(object):
 
-    def __init__(self, uri):
+    def __init__(self, uri, timeout):
         self._mtimes = {}
-        self._client = nrepl_connect(uri)
+        self.uri = uri
+        self.timeout = timeout
 
     def trigger_reload(self, filename):
         filename = os.path.realpath(filename)
@@ -131,23 +140,91 @@ class Watcher(object):
                 self._mtimes[filename] = mtime
                 self.trigger_reload(filename)
 
-    def start(self, dirpath, interval=1):
+    def start(self, dirpath, interval=3):
+        start_ts = get_ts()
+        while True:
+            try:
+                self._client = nrepl_connect(self.uri)
+                break
+            except socket.error, e:
+                eno, _ = e
+                if eno == errno.ECONNREFUSED:
+                    ts = get_ts()
+                    if (ts - start_ts) > self.timeout:
+                        raise TimeoutError('%s (in %ss)' % (e, self.timeout))
+                    else:
+                        continue
+                raise e
+            time.sleep(interval)
+
+        print("Started watching", dirpath)
+
         while True:
             traverse_dir(dirpath, self.reload_if_updated)
             time.sleep(interval)
 
 
+class Handler(threading.Thread):
+
+    def __init__(self, cmd):
+        threading.Thread.__init__(self)
+        self._cmd = cmd
+        self._process = None
+
+    def run(self):
+        cmd = ' '.join(self._cmd)
+        self._process = subprocess.Popen(cmd, shell=True,
+                                         stdout=subprocess.PIPE,
+                                         stderr=subprocess.STDOUT,
+                                         preexec_fn=os.setsid)
+        for line in iter(self._process.stdout.readline, b''):
+            print (line.rstrip())
+
+    def stop(self):
+        if self._process is not None:
+            os.killpg(os.getpgid(self._process.pid), signal.SIGTERM)
+            self._process = None
+
+
+def build_sig_handler(proc):
+    def sig_handler(signum, frame):
+        proc.stop()
+        proc.join()
+        sys.exit(0)
+    return sig_handler
+
+
 def cli():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--path', default='.')
+    parser.add_argument('cmd', choices=['run', 'watch'])
     parser.add_argument('-u', '--uri', default=DEFAULT_nREPL_URI)
+    parser.add_argument('-p', '--path', default='.')
+    parser.add_argument('-t', '--timeout', type=int, default=60)
     args = parser.parse_args()
 
     path = os.path.realpath(args.path)
 
-    watcher = Watcher(args.uri)
-    print("Start watching", path)
-    watcher.start(path)
+    if args.cmd == 'run':
+        h = Handler(['lein', 'run'])
+        h.start()
+
+        signal.signal(signal.SIGTERM, build_sig_handler(h))
+        signal.signal(signal.SIGINT, build_sig_handler(h))
+
+        time.sleep(5)    # wait for start
+
+        try:
+            watcher = Watcher(args.uri, timeout=args.timeout)
+            watcher.start(path)
+            print("Started watching", path)
+        except Exception as e:
+            h.stop()
+            h.join()
+            raise e
+
+    if args.cmd == 'watch':
+        watcher = Watcher(args.uri, timeout=args.timeout)
+        watcher.start(path)
 
 
 if __name__ == '__main__':
