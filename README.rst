3475dabdcd5112a8424d61fa9e38b4156e43c6c1
shonenada <shonenada@gmail.com>
2017-02-23 01:26:37 +0800

    chore(base): change README.md to README.rst; update setup.py

---
+++ README.rst
@@ -0,0 +1,17 @@
+nREPL-reloader
+==============
+
+Auto reload class with `nREPL <(https://github.com/clojure/tools.nrepl>`_.
+
+Quick Start
+-----------
+
+::
+
+    $ pip install nrepl
+
+::
+
+    $ nrepl watch -p /path/to/project -u nrepl://127.0.0.1:59258
+    Start watching /home/shonenada/Projects/clojure/src
+    Detected changes of /home/shonenada/Projects/clojure/src/route.clj, reloading
--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
+with open(os.path.join(CURRENT_DIR, "README.rst")) as long_description_file:
+    long_description = long_description_file.read()
 
 setup(
-    name="repl",
+    name="nrepl",
     version="0.1.0",
     url="https://github.com/shonenada/nREPL-reloader",
     author="shonenada",
+    author_email="shonenada@gmail.com",
+    description="Auto reload class with Clojure nREPL",
+    long_description=long_description,
     platforms='any',
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
             'nrepl = nrepl.core:cli',
@@ -18,6 +24,13 @@ setup(
     },
     classifiers=[
         'Environment :: Console',
-        'Topic :: System :: Shells'
+        'Topic :: System :: Shells',
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
     ]
 )
